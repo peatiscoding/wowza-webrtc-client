cbbefef08ad3990f91f2eb3fdeb187597803b397
Kittiphat S <freeuxer@gmail.com>
2020-04-19 13:10:17 +0700

    Correct h264 force mechanic

--- lib/publisher/SDPMessageProcessor.ts
+++ lib/publisher/SDPMessageProcessor.ts
@@ -127,7 +127,7 @@ export class SDPMessageProcessor {
    */
   private forceH264(sdp: string): string {
     cnsl.log(`Forcing SDP: ${sdp}`)
-    return sdp.replace(/profile-level-id=(42001f|64C016)/i, '42e01f')
+    return sdp.replace(/(profile-level-id)=(42001f|64C016)/i, '$1=42e01f')
         // .replace(/([\r\n]{2})[^=]+([a-z]=)/g, '$1$2')
   }
 
